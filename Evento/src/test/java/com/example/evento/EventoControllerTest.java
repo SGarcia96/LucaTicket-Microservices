70bb82e85c8ce2e05468540f5e0197a1e56df8b0
SGarcia96 <sgcerveradev@gmail.com>
2022-01-25 18:16:11 +0100

    add post test of eventControllerTest

--- Evento/src/test/java/com/example/evento/EventoControllerTest.java
+++ Evento/src/test/java/com/example/evento/EventoControllerTest.java
@@ -3,6 +3,8 @@ package com.example.evento;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.example.evento.model.Evento;
 import com.example.evento.model.Recinto;
@@ -14,6 +16,8 @@ import static org.hamcrest.Matchers.greaterThan;
 import java.util.Date;
 
 public class EventoControllerTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EventoControllerTest.class);
 	
 	@BeforeAll
 	public static void setup() {
@@ -34,19 +38,23 @@ public class EventoControllerTest {
 	}
 	
 	// POST /eventos
+	@SuppressWarnings("deprecation")
 	@Test
 	@Disabled
 	public void shouldAddEventoWithStatus201() {
+		logger.info("eeeeeeeeeeeeeeeeeeeeeeeeee");
 		Evento evento = new Evento();
+		
 		evento.setNombre("ntest");
 		evento.setDescripcionCorta("dcorta");
 		evento.setDescripcionLarga("dlarga");
 		evento.setFotoUrl("m.jpg");
-		evento.setFechaEvento(new Date());
+		evento.setFechaEvento(new Date(2000,04,12));
 		evento.setHoraEvento("20:00");
 		evento.setPoliticaAcceso("pacc");
 		evento.setRangoPrecios(new float[] {(float) 1.1, (float) 2.2});
 		evento.setRecinto(new Recinto("a", "b", "c", 10));
+		logger.info(evento.toString());
 			
 		given()
 			.contentType("application/json")
@@ -54,7 +62,8 @@ public class EventoControllerTest {
 		.when()
 			.post()
 		.then()
-			.statusCode(201);
+			.statusCode(201)
+			.body("nombre", equalTo("ntest"));
 				
 	}
 }
