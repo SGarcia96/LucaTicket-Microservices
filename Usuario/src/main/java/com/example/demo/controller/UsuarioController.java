0c3bece5746897fed83a007140d2d7fdaaec545e
EvaMontiel <evamontielaraque@gmail.com>
2022-01-25 10:04:37 +0100

    fix controller

--- Usuario/src/main/java/com/example/demo/controller/UsuarioController.java
+++ Usuario/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.usuario.model.Usuario;
+
 
 
 @Controller
