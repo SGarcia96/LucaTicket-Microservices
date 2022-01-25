5792cb11819be4872c71230733d52804045a96b3
Josema017 <jmom1991@gmail.com>
2022-01-25 12:14:31 +0100

    sexto commit

--- Usuario/src/main/java/com/example/usuario/service/UsuarioService.java
+++ Usuario/src/main/java/com/example/usuario/service/UsuarioService.java
@@ -1,8 +1,12 @@
 package com.example.usuario.service;
 
+import java.util.List;
+
 import com.example.usuario.model.Usuario;
 
 public interface UsuarioService {
 
 	public Usuario save(Usuario usuario);
+	
+	public List<Usuario> findAll();
 }
