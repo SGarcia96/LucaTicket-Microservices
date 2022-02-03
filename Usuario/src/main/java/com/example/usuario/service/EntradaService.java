6b8279be32ec644f0c725e5e9e48f379c44c93a9
EvaMontiel <evamontielaraque@gmail.com>
2022-02-03 12:27:27 +0100

    fix error aforo

--- Usuario/src/main/java/com/example/usuario/service/EntradaService.java
+++ Usuario/src/main/java/com/example/usuario/service/EntradaService.java
@@ -1,19 +1,19 @@
 package com.example.usuario.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.example.usuario.model.Entrada;
 import com.example.usuario.model.EventoDTO;
-import com.example.usuario.model.MensajePago;
 
 @Service
 public interface EntradaService {
 
 	public List<Entrada> findAll();
 
-	public MensajePago addEntrada(Long idUsuario, String idEvento);
+	public Map<String, Object> addEntrada(Long idUsuario, String idEvento);
 
 	public Entrada saveEntrada(Long idUsuario, EventoDTO evento);
 
